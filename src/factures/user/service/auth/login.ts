0bd955054f956f3ecb17f6b918c12a8879f626d4
david123456858 <juandavidperaltafuentes@gmail.com>
2025-08-26 21:53:19 -0500

    error por corregir

--- src/factures/user/service/auth/login.ts
+++ src/factures/user/service/auth/login.ts
@@ -2,14 +2,16 @@ import { pathData } from '../../../../config/route/route'
 import { IFailureProcess, ISuccessProcess } from '../../../../interfaces/resutl/result'
 import { fileUtils } from '../../../../utils/file/file'
 import { FailureProcess, SuccessProcess } from '../../../../utils/result/result'
+
 import path from 'path'
+import { RepositoryUser } from '../../repository/user'
 
 export class ServiceAuthLogin {
   private readonly path: string
   private readonly classUtilsFiles: fileUtils
   constructor () {
     this.path = path.join(process.cwd(), pathData.USERS, '/user.json')
-    this.classUtilsFiles = new fileUtils(this.path)
+    this.classUtilsFiles = new RepositoryUser(this.path)
   }
 
   login (dto?: string): IFailureProcess<any> | ISuccessProcess<any> {
