6e823ba5f73e51062b48f7a1f3559ce6b2699840
david123456858 <juandavidperaltafuentes@gmail.com>
2025-08-26 20:30:42 -0500

    realizando paths

--- src/config/route/route.ts
+++ src/config/route/route.ts
@@ -3,3 +3,8 @@ import { Router } from 'express'
 export const route = Router()
 
 export const routeBase = '/api/v1'
+
+export const pathData = {
+  USERS: 'src/data/users',
+  TACKS: 'src/data/taks'
+}
--- src/factures/user/service/auth/login.ts
+++ src/factures/user/service/auth/login.ts
@@ -1,17 +1,20 @@
+import { pathData } from '../../../../config/route/route'
 import { IFailureProcess, ISuccessProcess } from '../../../../interfaces/resutl/result'
 import { fileUtils } from '../../../../utils/file/file'
 import { FailureProcess, SuccessProcess } from '../../../../utils/result/result'
+import path from 'path'
 
 export class ServiceAuthLogin {
   private readonly path: string
   private readonly classUtilsFiles: fileUtils
   constructor () {
-    this.path = '../../../../data/users/user.json'
+    this.path = path.join(process.cwd(), pathData.USERS, '/user.json')
     this.classUtilsFiles = new fileUtils(this.path)
   }
 
   login (dto?: string): IFailureProcess<any> | ISuccessProcess<any> {
     try {
+      console.log(this.path)
       return SuccessProcess('Llegue señores tiurrrriririri', 200)
     } catch (error) {
       return FailureProcess('', 500)
